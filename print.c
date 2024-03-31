455a2ca4d87790e0aa10ac1ba8bcca39c9fcdd8e
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-03-31 23:32:18 +0600

    tried to learn basic c

---
+++ print.c
@@ -0,0 +1,43 @@
+// #include <stdio.h>
+// int main()
+// {
+//     printf("This is Al Amin Miah.\nI want to be a programmer.");
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     printf("Habijabi: !@#$^&*+_=-,.,");
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     // printf("\\");
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     printf("100%%");
+//     return 0;
+// }
+
+/*
+special character: (ESCAPE)
+
+1. \n -- new line
+2. \t -- tab
+*/
+
+/*
+Data types in C:
+1. int
+2. float
+3. char
+4. bool
+*/
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+// int main()
+// {
+//     printf("This is Al Amin Miah.\nI want to be a programmer.");
+//     return 0;
+// }
