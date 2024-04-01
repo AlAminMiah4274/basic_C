2ff709f86d0541b6cc0c97cc2040eb6f9e74b03a
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-04-01 20:20:38 +0600

    learnt about variable

--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-// int main()
-// {
-//     printf("This is Al Amin Miah.\nI want to be a programmer.");
-//     return 0;
-// }
---
+++ variable.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+
+    // int rahim = 100, karim = 500;
+    // int rahim = 100;
+    // int karim = 500;
+    // printf("%d %d", rahim, karim);
+
+    // float chol = 10.25, rent = 1.5;
+    // printf("%0.2f %0.1f", chol, rent);
+
+    // char name = 'A', what = 'Y';
+    char name = 'A';
+    char what = 'Y';
+    printf("%c %c", name, what);
+    return 0;
+}
+
+/*
+Format specifier: 
+1. %d - int
+2. %f - float
+3. %c - char
+*/
