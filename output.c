de2eaac9d8e5c027c889923357bd8e5f703b2e8f
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-04-01 23:26:46 +0600

    learnt taking input

---
+++ output.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{   
+    // int Rahim;
+    // float CGPA;
+    // char Grade;
+    // scanf("%d %f %c", &Rahim, &CGPA, &Grade);
+    // printf("Output is: %d %0.2f %c", Rahim, CGPA, Grade);
+
+    // int num1, num2;
+    // scanf("%d%% %d%%", &num1, &num2);
+    // printf("Output is: %d%% %d%%", num1, num2);
+
+    // int num1, num2;
+    // char p;
+    // scanf("%d%c %d%c", &num1, &p, &num2, &p);
+    // printf("output is: %d%% %d%%", num1, num2);
+
+    int a, b;
+    scanf("%da %db", &a, &b);
+    printf("output is: %da %db", a, b);
+    return 0;
+}
