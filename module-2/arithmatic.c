074ff3568733e334f92091bb918f4060100161a4
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-04-02 23:41:22 +0600

    done arithmatic operators

---
+++ module-2/arithmatic.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    // int a = 57, b = 54;
+    // int sum = a + b;
+    // printf("%d", sum);
+
+    // int a = 20, b= 23;
+    // int subtraction = a - b;
+    // printf("%d", subtraction);
+
+    // int a = 4, b = 20;
+    // int multiplication = a * b;
+    // printf("%d", multiplication);
+
+    // int a = 40, b = 14;
+    // int result = a / b;
+    // printf("%d", result);
+
+    int a = 23, b = 9;
+    float result = a * 1.0 / b;
+    printf("%f", result);
+
+    // int a = 40, b = 13;
+    // int result = a % b;
+    // printf("%d", result);
+    return 0;
+}
+
+/*
+Arithmatic operators: +, -, *, /, %
+*/
