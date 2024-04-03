78fdb50f4e3dad89fbdef48fd0c4ea08e7b0e54c
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-04-03 22:58:01 +0600

    solved third practice problem

---
+++ module_2.5/practice_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    scanf("%d", &number);
+
+    if(number % 2 == 0){
+        printf("It is a even number.");
+    }
+    else{
+        printf("It is a odd number.");
+    }
+
+    return 0;
+}
