58c97fc6c26068606c6f7b0a9f6c1f4523fc0f39
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-04-03 21:46:38 +0600

    solved first practice problem

---
+++ module_2.5/problem_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("Recently I heard that you have achieved 95%% marks in your exam.\n");
+    printf("This is brilliant!\n");
+    printf("I wish you'll shine in your life!\tGood luck with all the barriars (/\\) in your life.");
+
+    return 0;
+}
