7740bc2d45982f9c3ca0364610b7e769ff2767e8
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-04-03 18:07:27 +0600

    operators in c

---
+++ module-2/relational.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 10;
+    int result = a >= b;
+    printf("%d", result);
+    return 0;
+}
+
+/*
+Relational opearators: >, <, !=, ==, >=, <= 
+*/
