42a3a56564b98df78dcb87b7f15f6d7b462a02f4
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-04-05 00:00:56 +0600

    practiced quizes code

---
+++ module_3/quize.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    // int k;
+    // for (k = -3; k < -5; k++)
+    // {
+    //     printf("Hello ");
+    // }
+
+    // int i = 0;
+    // while (i < 3)
+    // {
+    //     i++;
+    // }
+    // printf("%d\n", i);
+
+    // int a = 0, i;
+    // for (i = 0; i < 5; i++)
+    // {
+    //     a++;
+    //     continue;
+    // }
+    // printf("%d %d", i, a);
+
+    // int a = 0, i;
+    // for (i = 0; i < 5; i++)
+    // {
+    //     continue;
+    //     i++;
+    // }
+    // printf("%d %d", i, a);
+
+    int i = 0;
+    while (i = 0)
+    {
+        printf("Hello\n");
+    }
+
+    return 0;
+}
