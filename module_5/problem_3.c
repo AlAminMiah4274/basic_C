3443c6051b2d0d170af0a184ca96c09a7e2173db
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-04-07 14:54:19 +0600

    solved problem 3

--- module_5/problem_3.c
+++ module_5/problem_3.c
@@ -4,5 +4,16 @@ int main()
     char x;
     scanf("%c", &x);
 
+    if (x >= 65 && x <= 90)
+    {
+        int lower = x + 32;
+        printf("%c", lower);
+    }
+    else 
+    {
+        int upper = x - 32;
+        printf("%c", upper);
+    }
+    
     return 0;
 }
