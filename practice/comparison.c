74541867692bcfed5695e88701e83c7c8764290f
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-04-08 22:37:37 +0600

    solved summation of 1 to N problem

--- practice/comparison.c
+++ practice/comparison.c
@@ -8,13 +8,24 @@ int main()
     // int divisor = d / Q;
     // printf("%d", divisor);
 
-    int x, y;
-    scanf("%d %d", &x, &y);
+    // int x, y;
+    // scanf("%d %d", &x, &y);
 
-    int both = x - y;
-    int rina = both / 2;
-    int mina = both / 2 + y;
-    printf("%d %d", mina, rina);
+    // int both = x - y;
+    // int rina = both / 2;
+    // int mina = both / 2 + y;
+    // printf("%d %d", mina, rina);
+
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    int i;
+    for (i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    
+    printf("%d", sum);
 
     return 0;
 }
