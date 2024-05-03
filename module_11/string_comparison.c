d22d161f6569381fe28fe452714a7883db60a06a
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-05-03 23:17:32 +0600

    tried to learn string comparison

---
+++ module_11/string_comparison.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char string_one[100], string_two[100];
+    scanf("%s %s", string_one, string_two);
+
+    int i = 0;
+    while (1)
+    {
+        if (string_one[i] == '\0' && string_two[i] == '\0')
+        {
+            printf('same\n');
+            break;
+        }
+        else if (string_one[i] == '\0')
+        {
+            printf("string one is small\n");
+        }
+        else if (string_two[i] == '\0')
+        {
+            printf("string two is small\n");
+        }
+
+        if (string_one[i] == string_two[i])
+        {
+            i++;
+        }
+        else if (string_one[i] < string_two[i])
+        {
+            printf("string one small");
+            break;
+        }
+        else 
+        {
+            printf("string two is small");
+            break;
+        }
+    }
+
+    return 0;
+}
